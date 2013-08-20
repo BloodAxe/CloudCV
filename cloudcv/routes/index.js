028782f282cf3a041d808fc2a03041a2963ee427
IEVGEN KHVEDCHENIA <ekhvedchenya@gmail.com>
2013-08-20 17:46:06 +0300

    Move OpenCV module to node_modules folder

--- cloudcv/routes/index.js
+++ cloudcv/routes/index.js
@@ -3,6 +3,12 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var cv = require('cv');
+
+exports.index = function(req, res)
+{
+  res.render('index', 
+  	{ title: 'Express', 
+  	  opencv_info: cv.buildInformation() 
+  	});
 };
