20df5a2c09cb191b91fb35d89ea1b123ef9e557a
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2013-09-05 21:40:25 +0300

    Update layout

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@
  * GET home page.
  */
 
-var cv = require('../node_modules/cloudcv/build/Release/cloudcv');
-
 exports.index = function(req, res)
 {
   res.render('index', 
   	{ title: 'Express', 
-  	  opencv_info: cv.buildInformation()
+  	  opencv_info: ""//cv.buildInformation()
   	});
 };
