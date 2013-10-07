ffa2f70c9ec12b7bc7ecb1d8992a88158c62ed43
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2013-10-07 08:38:34 +0000

    Included support of the HTTPS protocol

--- app.js
+++ app.js
@@ -4,4 +4,20 @@
 
 var app = require("./server.js");
 
-app.listen(80);
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+
+// This line is from the Node.js HTTPS documentation.
+var options = 
+{
+  ca: fs.readFileSync('/etc/ssl/cloudcv/GandiStandardSSLCA.pem'),
+  key: fs.readFileSync('/etc/ssl/cloudcv/cloudcv-key.pem'),
+  cert: fs.readFileSync('/etc/ssl/cloudcv/cloudcv-cert.pem')
+};
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+
+//app.listen(80);
