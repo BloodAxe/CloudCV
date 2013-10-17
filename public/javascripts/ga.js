57b7b3d47d0f0950dc4c0f478eb280d134b1c692
IEVGEN KHVEDCHENIA <ekhvedchenya@gmail.com>
2013-10-17 08:41:13 +0300

    Update google analytics tracking code

--- public/javascripts/ga.js
+++ public/javascripts/ga.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-10687369-9', 'cloudcv.io');
+  ga('create', 'UA-10687369-8', 'cloudcv.io');
   ga('send', 'pageview');
