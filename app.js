38b24f3e319a51433c7f11c40537ce6bd495b5af
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2014-02-24 10:34:03 +0200

    Added checking for the presence of SSL certificate

--- app.js
+++ app.js
@@ -9,19 +9,31 @@ var http = require('http');
 var fs = require('fs');
 
 
-// This line is from the Node.js HTTPS documentation.
-var options = 
-{
-  ca: fs.readFileSync('/etc/ssl/cloudcv/GandiStandardSSLCA.pem'),
-  key: fs.readFileSync('/etc/ssl/cloudcv/cloudcv-key.pem'),
-  cert: fs.readFileSync('/etc/ssl/cloudcv/cloudcv-cert.pem')
-};
 
 var http_server  = http.createServer(app);
-var https_server = https.createServer(options, app);
+var https_server = null;
+
+var ca = '/etc/ssl/cloudcv/GandiStandardSSLCA.pem';
+var key = '/etc/ssl/cloudcv/cloudcv-key.pem';
+var cert = '/etc/ssl/cloudcv/cloudcv-cert.pem';
+
+if (fs.existsSync(ca) && fs.existsSync(key) && fs.existsSync(cert)) {
+
+    var options = 
+    {
+      ca: fs.readFileSync(ca),
+      key: fs.readFileSync(key),
+      cert: fs.readFileSync(cert)
+    };
+
+    https_server = https.createServer(options, app);
+}
 
 var sockets = require("./routes/socket.js");
 sockets.bindTo(http_server);
 
 http_server.listen(80);
-https_server.listen(443);
+
+if (https_server) {
+    https_server.listen(443);    
+}
