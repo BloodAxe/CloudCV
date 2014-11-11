8d758d4e7fe485ceaacb1c372e53e9ecb9c31243
BloodAxe <ekhvedchenya@gmail.com>
2014-11-11 22:00:53 +0200

    No more troubles with SSL, use Nginx as public web-server that proxy all traffic to node.

--- app.js
+++ app.js
@@ -2,35 +2,10 @@
  * Module dependencies.
  */
 
-var app = require("./server.js");
+var express = require('express');
+var app     = require("./server.js");
 
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
+app.use(express.errorHandler());
+var server  = require('http').createServer(app);
 
-
-
-var http_server  = http.createServer(app);
-var https_server = null;
-
-var ca = '/etc/ssl/cloudcv/GandiStandardSSLCA.pem';
-var key = '/etc/ssl/cloudcv/cloudcv-key.pem';
-var cert = '/etc/ssl/cloudcv/cloudcv-cert.pem';
-
-if (fs.existsSync(ca) && fs.existsSync(key) && fs.existsSync(cert)) {
-
-    var options = 
-    {
-      ca: fs.readFileSync(ca),
-      key: fs.readFileSync(key),
-      cert: fs.readFileSync(cert)
-    };
-
-    https_server = https.createServer(options, app);
-}
-
-http_server.listen(80);
-
-if (https_server) {
-    https_server.listen(443);    
-}
+server.listen(8888, 'localhost');
